2186a63311c902296c87ab42994415b82e14b45a
picpal <picpal@kakao.com>
2023-12-19 21:34:14 +0900

    add export data function

--- src/components/AccountMenu.jsx
+++ src/components/AccountMenu.jsx
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UploadSimple from '../assets/images/icon/UploadSimple.png'
 import DownloadSimple from '../assets/images/icon/DownloadSimple.png'
 import ArrowsCounterClockwise from '../assets/images/icon/ArrowsCounterClockwise.png'
+import IndexedDBManager from '../api/IndexedDBManager';
 
 
 const AccountMenu = ({showMenu}) => {
+  const [dbManager , setDbManager] = useState();
+
+  useEffect(() => {
+    console.log('one')
+    const indexedDBManager = new IndexedDBManager();
+    setDbManager(indexedDBManager);
+  }, []);
+
   const clickHandlerImportData = () => {
+    
     alert('Upload File');
   }
 
-  const clickHandlerExportData = () => {
-    alert('Download File');
+  const clickHandlerExportData = async () => {
+    const data = await dbManager.getDataAll('accountList');
+    console.log(JSON.stringify(data))
+    const blob = new Blob([JSON.stringify(data)], { type: "text/plain" });
+    const url = window.URL.createObjectURL(blob);
+
+    // a태그를 이용하여 blob 객체 호출
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = "account_backup";
+    document.body.appendChild(a);
+
+    // 트리거
+    a.click();
+
+    // 다운로드 후 a 태그를 제거하고, Blob URL을 해제합니다.
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
   }
 
   const clickHandlerResetData = () => {
-    alert('Reset Data');
+    if(window.confirm("모든 데이터를 삭제 하시겠습니까?")){
+      dbManager.clearDatabase('accountList');
+    }
   }
 
   return(
